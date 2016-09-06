2064aea72dd16beb4a956f68371dc982234069a6
sagar <sagarcuet@yahoo.com>
2016-09-06 16:56:31 +0600

    Update administrator.php

--- administrator.php
+++ administrator.php
@@ -1,6 +1,7 @@
 <?php include "header.php" ?>
 <?php include "menu_administrator.php" ?>
 	 <div class="content">
+	 	There is something new in git source.
      </div>
 <?php include "footer.php" ?>
 
